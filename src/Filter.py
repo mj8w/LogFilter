346edb26d77a4eab59f63dfeaed88e2c8ac9f177
Mike Wilson <v-michew@microsoft.com>
2020-05-05 12:18:07 -0700

    imprvements I didn't document. sigh.

--- src/Filter.py
+++ src/Filter.py
@@ -19,11 +19,21 @@ class Application(tk.Frame):
         self.available_markers = marker_options
         self.pack()
         self.create_widgets()
-        self.file_contents = ""
+        self.file_contents = []
+        self.line_map = []          # for each filtered line displayed, the original line #  
         
     def LoadFileWithFilter(self):
         try:
-            file_contents = []
+            total_lines = len(self.file_contents)
+            first, last = [val*total_lines for val in self.textbox.yview()]
+            if last < 1: 
+                first_to_show = 1
+            else: 
+                first_to_show = self.line_map[int(first)]            # line number to align display to after it is loaded
+            print("first to show {}".format(first_to_show))
+            
+            self.file_contents = []
+            self.line_map = [] 
             element = str( self.Filter_Re.get())
             marking = str(self.Mark_Re.get())
             print("Element selected '{}'".format(element))
@@ -39,10 +49,12 @@ class Application(tk.Frame):
                     if keep:
                         if self.HaveLineNums.get() == "on":
                             line = " ".join([lineNumStr, line])
-                            file_contents.append(line)
+                            self.file_contents.append(line)
+                            self.line_map.append(lineNum)
                             self.textbox.AddLineNumTag(locateLineNum,0,locateLineNum,5)
                         else:
-                            file_contents.append(line)
+                            self.file_contents.append(line)
+                            self.line_map.append(lineNum)
                         
                         for will_mark in re.finditer(marking, line):
                             print(locateLineNum,will_mark.start(),locateLineNum,will_mark.end())
@@ -51,10 +63,29 @@ class Application(tk.Frame):
                         locateLineNum += 1
                     lineNum += 1
                         
-                self.textbox.LoadData(file_contents)
+                self.textbox.LoadData(self.file_contents)
+                
+                for i,lineNum in enumerate(self.line_map):
+                    if lineNum >= first_to_show:
+                        first = float(i)/float(len(self.line_map))
+                        print("Restore show {} ({} of {} lines".format(first, i, len(self.line_map)))
+                        # self.textbox.see("{}.{}".format(i,0))
+                        self.textbox.yview_moveto(first)
+                        break
                 
         except AttributeError:
             pass
+
+        """
+        from ScrolledText import ScrolledText
+        s=ScrolledText()
+        s.pack(fill='both', expand=1)
+        s.insert('end', some_text)
+        first, last = s.yview()
+        s.delete(1.0, 'end')
+        s.insert('end', some_text)
+        s.yview_moveto(first)
+        """
 
     def GetFileName(self):
         self.filename= fd.askopenfilename(filetypes=(("Logs", ".log"), ("All files","*.*"))) 
@@ -114,7 +145,7 @@ class Application(tk.Frame):
         self.scroll = tk.Scrollbar(self.textbox_frame, command = self.textbox.yview)
         self.textbox.configure(yscrollcommand=self.scroll.set)
         
-        self.linenumbers = TextLineNumbers(self.textbox_frame, width=30)
+        self.linenumbers = TextLineNumbers(self.textbox_frame, width=40)
         self.linenumbers.attach(self.textbox)
 
         # place the scrollbar next to the text box
@@ -125,69 +156,67 @@ class Application(tk.Frame):
         self.textbox.bind("<<Change>>", self._on_change)
         self.textbox.bind("<Configure>", self._on_change)
 
-    def create_filter_frame(self):
-        self.filter_frame = tk.Frame(self.master)
+    def create_widgets(self):
+
+        self.create_textbox_frame()
+        self.textbox_frame.pack(side=tk.BOTTOM, expand = tk.YES, fill=tk.BOTH)
+
+        # options frame goes on the top & contains a number of widgets
+        self.options_frame = tk.Frame(self.master, borderwidth=5)
+        self.options_frame.pack(side=tk.LEFT)
+
+        # ROW 0
 
         # file dialog button
-        self.file_select = tk.Button(self.filter_frame, text='File Open', command=self.GetFileName)
-        self.file_select.pack(side=tk.LEFT)
-                
-        # label
-        tk.Label(self.filter_frame, text="Filter Lines (RegEx)").pack(side=tk.LEFT)
-        
-        # Apply filters button
-        self.apply = tk.Button(self.filter_frame)
-        self.apply["text"] = "Apply"
-        self.apply["command"] = self.on_apply_filters
-        self.apply.pack(side=tk.RIGHT)
+        self.file_select = tk.Button(self.options_frame, text='File Open', command=self.GetFileName)
+        self.file_select.grid(row=0, column=0)
 
+        # label
+        tk.Label(self.options_frame, text="Filter Lines (RegEx)", padx = 5, pady = 5).grid(row=0, column=1)
+        
         # combo box
-        self.filter_re = ttk.Combobox(self.filter_frame, values=self.available_filters, textvariable = self.Filter_Re)
-        self.filter_re.pack(side=tk.RIGHT, fill=tk.X)
+        self.filter_re = ttk.Combobox(self.options_frame, values=self.available_filters, textvariable = self.Filter_Re)
+        self.filter_re.grid(row=0, column=2)
         self.filter_re.current(0)
         self.filter_re["width"] = 60
         self.filter_re.bind("<<ComboboxSelected>>", self.on_filter_combo_select)
         self.filter_re.bind("<Key>", self.on_keypress_filters)
 
-    def create_markers_frame(self):
-        self.markers_frame = tk.Frame(self.master)
+        # Apply filters button
+        self.apply = tk.Button(self.options_frame)
+        self.apply["text"] = "Apply"
+        self.apply["command"] = self.on_apply_filters
+        self.apply.grid(row=0, column=3)
+              
+        # ROW 1
+
+        # Linenum checkbox
+        self.HaveLineNums = tk.StringVar()
+        self.HaveLineNums.set("on")
+        self.LineNumCheck = ttk.Checkbutton(self.options_frame,
+        variable = self.HaveLineNums, 
+        text='Line #s',
+        command=self.OnHaveLineNumsChanged, onvalue='on', offvalue='off')
+        self.LineNumCheck.grid(row=1, column=0)
 
         # label
-        tk.Label(self.markers_frame, text="Mark Text (RegEx)").pack(side=tk.LEFT)
+        tk.Label(self.options_frame, text="Mark Text (RegEx)").grid(row=1, column=1)
         
-        # Apply markers button
-        self.apply = tk.Button(self.markers_frame)
-        self.apply["text"] = "Apply"
-        self.apply["command"] = self.on_apply_markers
-        self.apply.pack(side=tk.RIGHT)
-
         # combo box
-        self.marker_re = ttk.Combobox(self.markers_frame, values=self.available_markers, textvariable = self.Mark_Re)
-        self.marker_re.pack(side=tk.RIGHT, fill=tk.X)
+        self.marker_re = ttk.Combobox(self.options_frame, values=self.available_markers, textvariable = self.Mark_Re)
+        self.marker_re.grid(row=1, column=2)
         self.marker_re.current(0)
         self.marker_re["width"] = 60
         self.marker_re.bind("<<ComboboxSelected>>", self.on_marker_combo_select)
         self.marker_re.bind("<Key>", self.on_keypress_markers)
+        
+        # Apply markers button
+        self.apply = tk.Button(self.options_frame)
+        self.apply["text"] = "Apply"
+        self.apply["command"] = self.on_apply_markers
+        self.apply.grid(row=1, column=3)
+
                 
-    def create_widgets(self):
-              
-        # Linenum checkbox
-        self.HaveLineNums = tk.StringVar()
-        self.HaveLineNums.set("on")
-        self.LineNumCheck = ttk.Checkbutton(self.master,
-        variable = self.HaveLineNums, 
-        text='Incl. Line Numbers',
-        command=self.OnHaveLineNumsChanged, onvalue='on', offvalue='off')
-        self.LineNumCheck.pack(side=tk.BOTTOM)
-
-        self.create_filter_frame()
-        self.filter_frame.pack(side=tk.BOTTOM)
-
-        self.create_markers_frame()
-        self.markers_frame.pack(side=tk.BOTTOM)
-
-        self.create_textbox_frame()
-        self.textbox_frame.pack(side=tk.BOTTOM, expand = tk.YES, fill=tk.BOTH)
 
 root = tk.Tk()
 app = Application(master=root)
